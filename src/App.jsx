64b98df31f385be17c1c5e888edb9d4851b170ff
Rachel Boursia <91563448+rachelboursia@users.noreply.github.com>
2023-08-12 21:18:17 -0700

    Adds header and hero

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header/Header.jsx";
+import Hero from "./components/Hero/Hero.jsx";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <div className="white-gradient" />
+        <Header />
+        <Hero />
+      </div>
+    </div>
+  );
+}
+
+export default App;
